e2ff65716912e941c9ea7ebc1f637dec723c97a5
minegishi <>
2023-10-07 16:19:14 +0900

    Playerの動き追加

--- Assets/Minegishi/Scripts/PlayerController.cs
+++ Assets/Minegishi/Scripts/PlayerController.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public enum PlayerState { Human, Circle}
+    PlayerState playerstate;
+
+    [SerializeField] private float speed = 1.0f;
+
     void Start()
     {
-        
+        playerstate = PlayerState.Circle;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Run();
+        switch (playerstate)
+        {
+            case PlayerState.Human:
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    playerstate = PlayerState.Circle;
+                }
+                    Human();
+                break;
+            case PlayerState.Circle:
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    playerstate = PlayerState.Human;
+                }
+                Circle();
+                break;
+        }
+
+
+    }
+
+    void Human()
+    {
+        speed = 1.0f;
+        Debug.Log("êlÇ≈Ç∑");
+    }
+
+    void Circle()
+    {
+        speed = 3.0f;
+        Debug.Log("ãÖëÃÇ≈Ç∑");
+    }
+
+    void Run()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed, 0,0) * Time.deltaTime) ;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
     }
 }
