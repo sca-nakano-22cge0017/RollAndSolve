c257cd2a1a5925e4f61c634f005246c0731a5e91
goto <>
2023-10-14 18:12:34 +0900

    メニューシーンなど

---
+++ Assets/goto/Scrips/ActivateUIObj.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ActivateUIObj : MonoBehaviour
+{
+
+	//　画面UI
+	[SerializeField]
+	private GameObject statusWindow;
+	//　ボタンのインタラクティブに関する処理が書かれているスクリプト
+	[SerializeField]
+	private ActivateButton select1;
+	[SerializeField]
+	private ActivateButton select2;
+
+    
+
+    void Update()
+	{
+
+		//　スペースキーを押したら画面UIのオン・オフ
+		if (Input.GetKeyDown("space"))
+		{
+			statusWindow.SetActive(!statusWindow.activeSelf);
+
+			//　画面を開いた時にBackground1のボタンのインタラクティブをtrue、Background2のボタンのインタラクティブをfalseにする
+			if (statusWindow.activeSelf)
+			{
+
+				select1.ActivateOrNotActivate(true);
+				select2.ActivateOrNotActivate(false);
+
+				//　画面を閉じたら選択を解除
+			}
+			else
+			{
+				EventSystem.current.SetSelectedGameObject(null);
+			}
+		}
+	}
+}
