f0558bb62f3d8094bd113d9f1694987415c80da0
sca-goto-kosuke0006 <sdt22cge0006@edu.sca.ac.jp>
2023-10-28 15:24:22 +0900

    クリアシーンとゲームオーバーシーンの追加、UI関連修正

---
+++ Assets/goto/Scrips/MenuPanelOff.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPanelOff : MonoBehaviour
+{
+    [SerializeField] GameObject Panel;
+    [SerializeField] GameObject Panel1;
+
+    [SerializeField]
+    Button StartButton;
+
+    private void Start()
+    {
+       
+    }
+  
+    public void OnClick()
+    {
+
+       
+        var isActive = Panel.activeInHierarchy; // Panelがアクティブか取得
+        Panel.SetActive(!isActive);
+
+        var isActivee = Panel.activeInHierarchy; // Panelがアクティブか取得
+        Panel1.SetActive(!isActivee);
+        //EventSystem.current.SetSelectedGameObject(null);
+        StartButton.Select();
+       
+    }
+}
