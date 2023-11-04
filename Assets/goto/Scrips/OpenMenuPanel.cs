01adc686dd3b75bffdc5a91560656aa30142ef5d
goto <sdt22cge0006@edu.sca.ac.jp>
2023-11-04 14:20:23 +0900

    ゲーム終了。カーソル消す

--- Assets/goto/Scrips/OpenMenuPanel.cs
+++ Assets/goto/Scrips/OpenMenuPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuPanel : MonoBehaviour
+public class OpenMenuPanel : MonoBehaviour
 {
     [SerializeField] GameObject Panel;
     [SerializeField] GameObject Panel1;
@@ -11,17 +11,28 @@ public class MenuPanel : MonoBehaviour
     [SerializeField]
     Button StartButton;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab)
-        { 
-        Time.timeScale = 0;
+
+        
+    }
+
+    public void OnClick()
+    {
+
+
         var isActive = Panel.activeInHierarchy; // Panelがアクティブか取得
         Panel.SetActive(!isActive);
 
-        
+       
         StartButton.Select();
-        }
+
     }
 }
