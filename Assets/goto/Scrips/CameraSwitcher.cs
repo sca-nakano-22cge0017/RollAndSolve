505508e45ea5de3cd335c6b3f127af6119300f44
goto <sdt22cge0006@edu.sca.ac.jp>
2023-12-16 15:09:18 +0900

    カメラ追従一旦完了

--- Assets/goto/Scrips/CameraSwitcher.cs
+++ Assets/goto/Scrips/CameraSwitcher.cs
@@ -8,29 +8,64 @@ public class CameraSwitcher : MonoBehaviour
 {
     public CinemachineVirtualCameraBase vcam1;
     public CinemachineVirtualCameraBase vcam2;
+    public CinemachineVirtualCameraBase vcam3;
 
     [SerializeField]
     private bool _isSwitchOn;
     [SerializeField]
     private bool _isSwitchOff;
 
+    float timer;
+    float timer2;
+
     void Start()    
     {
-     _isSwitchOn = true;
+        timer = 0;
+        timer2 = 0;
+        _isSwitchOn = true;
      _isSwitchOff = false;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        Debug.Log(timer);
+        if (Input.GetKey(KeyCode.A))
         {
-            vcam1.Priority = 1;
-            vcam2.Priority = 0;
+            if (timer > 0.5f)
+            {  
+                vcam1.Priority = 1;
+                vcam2.Priority = 0;
+                vcam3.Priority = 0;
+            }
+            else
+            {
+                timer += Time.deltaTime;  //タイマー加算
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            vcam1.Priority = 0;
-            vcam2.Priority = 1;
+            if (timer2 > 0.5f)
+            { 
+                vcam1.Priority = 0;
+                vcam2.Priority = 1;
+                vcam3.Priority = 0;
+                Debug.Log("タイム");
+            }
+            else
+            {
+                timer2 += Time.deltaTime;  //タイマー加算
+              
+            }
+           
+        }
+          
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            timer = 0.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+            timer2 = 0.0f;
         }
     }
-  
 }
